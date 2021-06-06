148e522ee7080656ba42557e611e1e7c8b9034c3
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-06-06 18:34:39 +0800

    Update SecurityLayout.tsx

--- src/layouts/SecurityLayout.tsx
+++ src/layouts/SecurityLayout.tsx
@@ -3,29 +3,29 @@ import { stringify } from 'querystring';
 import ProLayout, { DefaultFooter } from '@ant-design/pro-layout';
 import RightConent from '@/components/GlobalHeader/RightConent';
 
-
 export default ({ children }) => {
-    const { initialState, loading } = useModel('@@initialState');
-    const { userinfo, title, logo } = initialState;
-    const { pathname, search } = location;
-    const queryString = stringify({
-        redirect: pathname + search
-    });
+  const { initialState, loading } = useModel('@@initialState');
+  const { userinfo, title, logo } = initialState;
+  const { pathname, search } = location;
+  const queryString = stringify({
+    redirect: pathname + search,
+  });
 
-    if (!userinfo) {
-        return <Redirect to={`/login?${queryString}`} />;
-    }
+  if (!userinfo) {
+    return <Redirect to={`/login?${queryString}`} />;
+  }
 
-    return (
-        <ProLayout
-            title={title}
-            logo={logo}
-            loading={loading}
-            layout="top"
-            navTheme="light"
-            footerRender={() => <DefaultFooter copyright="Origins" links={[]} />}
-            rightContentRender={RightConent}
-        >
-            {children}
-        </ProLayout>);
-}
+  return (
+    <ProLayout
+      title={title}
+      logo={logo}
+      loading={loading}
+      layout="top"
+      navTheme="light"
+      footerRender={() => <DefaultFooter copyright="Mblocks" links={[]} />}
+      rightContentRender={RightConent}
+    >
+      {children}
+    </ProLayout>
+  );
+};
