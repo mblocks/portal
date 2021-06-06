bca13a90c2639b34e26da02573c1c99336cb8ddc
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-06-06 13:29:28 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,12 @@ async function getInitialData() {
   const data = await initialData();
   const apps = [];
   const routes = [];
-  if (data.user && data.user.apps) {
-    data.user.apps.forEach((v) => {
+  if (data.userinfo && data.userinfo.apps) {
+    data.userinfo.apps.forEach((v) => {
       apps.push({
         ...v,
         entry: `/load/${v.name}`,
-        props: { user: data.user },
+        props: { userinfo: data.userinfo },
       });
       routes.push({
         path: `/${v.name}`,
